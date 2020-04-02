f1d5a11e5fac9f004e32d55b29c815c1bc10c023
yoursshahnawaz <shahnawazgalaxy340@gmail.com>
2020-04-02 16:11:45 +0530

    Added MongoDB database

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products.js');
 const orderRoutes = require('./api/routes/orders.js');
+
+mongoose.connect(
+    "mongodb+srv://node-shop:" + 
+    process.env.MONGO_ATLAS_PW + 
+    "@node-rest-shop-j5jgj.mongodb.net/test?retryWrites=true&w=majority", {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    }
+);
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
