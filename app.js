0669b022e9343f29cae5b80151470130aca3cd25
yoursshahnawaz <shahnawazgalaxy340@gmail.com>
2020-04-06 22:38:37 +0530

    Adding User Signup

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products.js');
 const orderRoutes = require('./api/routes/orders.js');
+const userRoutes = require('./api/routes/user');
 
 mongoose.connect(
     "mongodb+srv://node-shop:" + 
@@ -38,6 +39,7 @@ app.use((req, res, next) => {
 // Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
